ec9d22d0e213b306dcb40e95503e63572cc83cc0
kocya <sample@test.co.jp>
2024-11-04 23:08:59 +0900

    log4net初期化処理の警告抑止

--- TestVsProject/Sample.cs
+++ TestVsProject/Sample.cs
@@ -8,7 +8,10 @@ namespace test_vs_project
 {
     public class Sample
     {
+#pragma warning disable CS8602, CS8604
         private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+#pragma warning restore CS8602, CS8604
+
         public int Add(int lhs, int rhs)
         {
             _logger.Info($"add {lhs}, {rhs}");
